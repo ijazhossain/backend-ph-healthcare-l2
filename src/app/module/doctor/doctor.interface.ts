cd041cdeca4e8acdafd785b968228a5eaf39fef1
ijazhossain <ijaz.hossain35@gmail.com>
2026-02-22 17:56:58 +0600

    doctor crud added

--- src/app/module/doctor/doctor.interface.ts
+++ src/app/module/doctor/doctor.interface.ts
@@ -1,7 +1,22 @@
-export interface IUpdateDoctorPayload {
+import { Gender } from "../../../generated/prisma/enums";
 
-  name?: string;
-  profilePhoto?: string;
-  contactNumber?: string;
-  address?:string;
+export interface IUpdateDoctorSpecialtyPayload {
+    specialtyId: string;
+    shouldDelete?: boolean;
+}
+export interface IUpdateDoctorPayload {
+    doctor?: {
+        name?: string;
+        profilePhoto?: string;
+        contactNumber?: string;
+        address?: string;
+        experience?: number
+        registrationNumber?: string;
+        gender?: Gender;
+        appointmentFee?: number;
+        qualification?: string;
+        currentWorkingPlace?: string;
+        designation?: string;
+    },
+    specialties?: IUpdateDoctorSpecialtyPayload[];
 }
--- src/app/module/doctor/doctor.route.ts
+++ src/app/module/doctor/doctor.route.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
 import { DoctorController } from "./doctor.controller";
 
-const router=Router();
-router.get("/",DoctorController.getAllDoctors);
+const router = Router();
+router.get(
+  "/",
 
-export const DoctorRoutes=router;
+  DoctorController.getAllDoctors,
+);
+router.get(
+  "/:id",
+
+  DoctorController.getDoctorById,
+);
+router.patch("/:id", DoctorController.updateDoctor);
+router.delete(
+  "/:id",
+
+  DoctorController.deleteDoctor,
+);
+
+export const DoctorRoutes = router;
