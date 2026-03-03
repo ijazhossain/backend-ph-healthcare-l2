65b334b473ce75ded10722447be09b24ceacd266
ijazhossain <ijaz.hossain35@gmail.com>
2026-03-03 13:56:07 +0600

    authentication added

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,24 @@ import { IndexRoutes } from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import cookieParser from "cookie-parser";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./app/lib/auth";
+import path from "path";
+import cors from "cors";
+import { envVars } from "./config/env";
 const app: Application = express();
+//ejs set up
+app.set("view engine","ejs");
+app.set("views",path.resolve(process.cwd(),`src/app/templates`));
+
+app.use(cors({
+  origin:[envVars.FRONTEND_URL,envVars.BETTER_AUTH_URL,"https://localhost:3000","http://localhost:5000"],
+  credentials:true,
+  methods:["GET","POST","PUT","DELETE","PATCH"],
+  allowedHeaders:["Content-Type","Authorization"]
+}))
+//auth route
+app.use("/api/auth",toNodeHandler(auth));
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
 
