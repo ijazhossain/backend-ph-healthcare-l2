e2c741a82009209e3804b95776f41eb78df81fa8
ijazhossain <ijaz.hossain35@gmail.com>
2026-03-11 13:28:55 +0600

    stripe added

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import path from "path";
 import cors from "cors";
 import { envVars } from "./config/env";
 import qs from "qs";
+import { PaymentController } from "./app/module/payment/payment.controller";
 const app: Application = express();
 //qs
 app.set("query parser", (str: string) => qs.parse(str));
@@ -17,13 +18,18 @@ app.set("query parser", (str: string) => qs.parse(str));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(process.cwd(), `src/app/templates`));
 //stripe webhook
-app.post(
+/* app.post(
   "/webhook",
   express.raw({ type: "application/json" }),
   async (req: Request, res: Response) => {
     console.log("webhook received", req.body);
     res.status(200).json({ received: true });
   },
+); */
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  PaymentController.handleStripeWebhookEvent
 );
 app.use(
   cors({
