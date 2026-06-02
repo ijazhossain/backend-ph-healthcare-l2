54234d5ca0e1de3c5b0b2ee2caabef41cdbd17aa
ijazhossain <ijaz.hossain35@gmail.com>
2026-06-02 16:14:35 +0600

    user role and status change added on admin

--- src/app/module/admin/admin.route.ts
+++ src/app/module/admin/admin.route.ts
@@ -7,17 +7,32 @@ import { updateAdminZodSchema } from "./admin.validation";
 
 const router = Router();
 
-router.get("/",
-    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
-    AdminController.getAllAdmins);
-router.get("/:id",
-    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
-    AdminController.getAdminById);
-router.patch("/:id",
-    checkAuth(Role.SUPER_ADMIN),
-    validateRequest(updateAdminZodSchema), AdminController.updateAdmin);
-router.delete("/:id",
-    checkAuth(Role.SUPER_ADMIN),
-    AdminController.deleteAdmin);
+router.get(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  AdminController.getAllAdmins,
+);
+router.get(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  AdminController.getAdminById,
+);
+router.patch(
+  "/:id",
+  checkAuth(Role.SUPER_ADMIN),
+  validateRequest(updateAdminZodSchema),
+  AdminController.updateAdmin,
+);
+router.delete("/:id", checkAuth(Role.SUPER_ADMIN), AdminController.deleteAdmin);
+router.patch(
+  "/change-user-status",
+  checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+  AdminController.changeUserStatus,
+);
+router.patch(
+  "/change-user-role",
+  checkAuth(Role.SUPER_ADMIN),
+  AdminController.changeUserRole,
+);
 
 export const AdminRoutes = router;
